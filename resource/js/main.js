a40e051c4689c195e7b19c219a15bdb386abc6eb
mdrefathossen <61592501+mdrefathossen@users.noreply.github.com>
2020-05-13 09:10:12 -0700

    Add files via upload

---
+++ resource/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    
+    //STICKY MENU
+    
+    $(".js--services-section").waypoint(function(direction){
+        
+        if (direction == "down"){
+            $("nav").addClass("sticky");
+            } else {
+            $("nav").removeClass("sticky");
+            }
+        
+    });
+    
+    //mixitup (portfolio section)
+ var mixer = mixitup(".container");
+    
+
+});
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
